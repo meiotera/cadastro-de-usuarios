956ae52bcaf94a707bc5abc44bba756b946c06b8
Renan Ferreira <renanfc.re@gmail.com>
2022-04-17 13:56:04 -0300

    JS modificado, fazendo verificação de usuários existentes. Estilos modificados

--- programa/app.js
+++ programa/app.js
@@ -9,6 +9,10 @@ function cadastrar(){
 
     validarDados(nome, pass, confirmePass);
 
+    inNome.value = '';
+    inPass.value = '';
+    inConfirmePass.value = '';
+
 }
 let btCadastrar = document.getElementById('btCadastrar');
 btCadastrar.addEventListener('click', cadastrar);
@@ -24,24 +28,57 @@ function validarDados(nome, password, confirmePass){
     erroSenha.textContent = '';
     erroConfirmPass.textContent = '';
 
+    let validaNome = true;
+    let validaPassword = true;
+    let validaConfirmPass = true;
 
     if (nome === ''){
         erroNome.textContent = `Favor informar um nome de usuário`;
+        validaNome = false;
     }    
+    
     if (password === ''){
         erroSenha.textContent = `Favor verificar digitação`;
+        validaPassword = false;
     }
     if (confirmePass !== password || confirmePass === ''){
         erroConfirmPass.textContent = 'Senhas não coincidem';
+        validaConfirmPass = false
     } 
 
-    verificarUsuario(nome)
+    if(!validaNome || !validaPassword || !validaConfirmPass){
+        return
+    }
+  
+    if(verificarUsuario(nome)){
+        alert('Usuario ja existe');
+        nome.focus();
+        return;
+    }
+
+    if(localStorage.getItem('usuarios')){
+        let usuario = localStorage.getItem('usuarios') + ',' + nome;
+
+        localStorage.setItem('usuarios', usuario)
+
+    }else {        
+        localStorage.setItem('usuarios', nome);
+       
+    }
+
 
 }
 
 
-function verificarUsuario(usuario){
+function verificarUsuario(usuarios){
     let existe = false;
 
-    
+    if(localStorage.getItem('usuarios')){
+        let usuario = localStorage.getItem('usuarios').split(',');
+
+        if(usuario.indexOf(usuarios.toString()) >= 0){
+            existe = true;
+        }
+    }
+    return existe;    
 }
